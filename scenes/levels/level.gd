36fd6bce3d3caa00c045cf0ab2364ea87c03b496
Verth <vertisixs@gmail.com>
2024-07-26 16:59:09 +0200

    Instructions, Multiple Levels, Exit Button

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,6 +3,11 @@ class_name ParentLevel
 
 func _ready():
 	Globals.object_amounts = [0,0,0,0]
+	
+func _process(_delta):
+	if Input.is_action_just_pressed('quit'):
+		get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+		get_tree().quit()
 
 func _on_player_shoot(pos: Vector2, type: PackedScene, create: bool, direction: Vector2):
 	var projectile = type.instantiate() as Area2D
@@ -16,4 +21,3 @@ func _on_projectile_body_entered(pos: Vector2, type: PackedScene):
 	var new_object = type.instantiate() as RigidBody2D
 	new_object.position = pos
 	$Objects.call_deferred("add_child", new_object)
-
